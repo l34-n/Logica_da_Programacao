2a11e9a74ac37c539b36a2d84e9f9bd18dd241be
l34-n <84137114+l34-n@users.noreply.github.com>
2023-03-07 22:24:34 -0300

    Update 2.8_(Condicional)_Temperatura.py

--- 2. Exercicios_Condicionais/2.8_(Condicional)_Temperatura.py
+++ 2. Exercicios_Condicionais/2.8_(Condicional)_Temperatura.py
@@ -13,3 +13,19 @@ isso, você deve construir um programa que leia a letra "C" ou "F" indicando em 
 informada uma temperatura. Em seguida o programa deve mostrar a temperatura na outra escala com
 duas casas decimais.                     
 ''')
+
+iTemperatura = input("Qual a Escala da Temperatura (C ou F): ")
+
+if iTemperatura == "C":
+    iCelsius = float(input("\nDigite a Temperatura em Celsius: "))
+
+    iCalculo_Celsius = (((iCelsius * 9) / 5) + 32)
+
+    print("Temperatura Equivalente em Celsius: ",format(iCalculo_Celsius,'.2f'),"\n")
+
+else:
+    iFahrenheit = float(input("\nDigite a Temperatura em Fahrenheit: "))
+
+    iCalculo_Fahrenheit = (((iFahrenheit - 32) * 5) / 9)
+
+    print("Temperatura Equivalente em Celsius: ",format(iCalculo_Fahrenheit,'.2f'),"\n")
